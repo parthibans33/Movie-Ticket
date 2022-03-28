bfac3123d9808efc3575c1c461b3715529c7cd5c
parthu33 <94122404+parthu33@users.noreply.github.com>
2022-03-28 13:16:02 +0530

    Create ticket.py

---
+++ ticket.py
@@ -0,0 +1,49 @@
+
+
+class Movies():
+        
+        def __init__(self,movie_name):
+              self.movie_name = movie_name    
+
+        def show(self):
+                print('************   Welcome to RAM Cinemas    *************')
+                print("****Those Movies are running in our Theater'**** ")
+           
+                for key,val in movie_name.items():
+                        print(f'{key}\t Tickets Available\t\t{val}')
+
+    
+        def book(self):
+                
+                try:
+                        movieb_name =input('Enter the Movie you want to watch:').upper()       
+                        for key in movie_name.keys():         
+                                if key == movieb_name: 
+                                        tickets_avail = movie_name[key]
+                                        ticket_book = int(input('Enter the Ticket :'))
+                                        tickets_avail = tickets_avail - ticket_book
+                                        print('Your Ticket has been booked')
+                                        print( f'Available Tickets {tickets_avail}')
+                                        
+                                '''else:
+                                        print('Enter Valid Movie Name!!!!')
+                                        break'''
+                except:
+                        print('Something Wrong!! ')
+        
+
+movie_name = {'BEAST':200,'RRR':150,'KGF':140,'ET':56,'VALIMAI':50}
+m = Movies(movie_name)
+m.show()
+m.book()
+
+
+
+
+
+
+            
+            
+            
+            
+            
